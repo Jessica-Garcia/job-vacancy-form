c8faa38084a10172cdc31421eb6433abe8c4a1e2
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-11-23 16:29:21 -0300

    style: code formatting - lint

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,36 +1,47 @@
-import React, { ChangeEvent, useEffect, useState } from "react"
-import { Input } from "../Input"
-import { IVacancyProps } from "../../interfaces/IVacancyProps"
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { Input } from '../Input'
+import { IVacancyProps } from '../../interfaces/IVacancyProps'
 import style from './style.module.css'
-import {v4 as uuidv4 } from 'uuid'
-import {FilePdf} from 'phosphor-react'
-import {VacancyPDF} from '../../documents/vacancy/VacancyPDF'
+import { v4 as uuidv4 } from 'uuid'
+import { FilePdf } from 'phosphor-react'
+import { VacancyPDF } from '../../documents/vacancy/VacancyPDF'
 
 type FormProps = {
-    selectedVacancy: IVacancyProps,
-    setList: (current: React.SetStateAction<IVacancyProps[]> | IVacancyProps[]) => void,
-    setSelectedVacancy: (current: React.SetStateAction<IVacancyProps> | IVacancyProps) => void
+  selectedVacancy: IVacancyProps
+  setList: (
+    current: React.SetStateAction<IVacancyProps[]> | IVacancyProps[],
+  ) => void
+  setSelectedVacancy: (
+    current: React.SetStateAction<IVacancyProps> | IVacancyProps,
+  ) => void
 }
 
-export const Form = ({selectedVacancy, setList, setSelectedVacancy}: FormProps) => {
-    const [vagaDoForm, setVacancy] = useState<IVacancyProps>({} as IVacancyProps)
+export const Form = ({
+  selectedVacancy,
+  setList,
+  setSelectedVacancy,
+}: FormProps) => {
+  const [vagaDoForm, setVacancy] = useState<IVacancyProps>({} as IVacancyProps)
 
-    /*
+  /*
         renderiza no form a vaga selecionada no select quando eu clico em determinada vaga no select
     */
-    useEffect(() => {
-        setVacancy(selectedVacancy)
-    },[selectedVacancy])
-    
-    /*
+  useEffect(() => {
+    setVacancy(selectedVacancy)
+  }, [selectedVacancy])
+
+  /*
         quando digitar algo no input, adiciona no input, selecionado pelo nome, o valor 
         que está sendo digitado nele, atribuindo todos os valores digitados na vaga com o setVacancy
     */
-    const handleDataChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setVacancy(previousValue => ({...previousValue, [e.target.name]: e.target.value}))
-    }
+  const handleDataChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setVacancy((previousValue) => ({
+      ...previousValue,
+      [e.target.name]: e.target.value,
+    }))
+  }
 
-    /*
+  /*
         Quando for enviar os dados do form
         Se a vaga que está no form tem id, ou seja, se já existe no select, 
         cria-se uma nova lista, percorre-se a lista de vagas já existente e se o id da vaga no form, 
@@ -43,82 +54,133 @@ export const Form = ({selectedVacancy, setList, setSelectedVacancy}: FormProps) 
         e adiciona a nova lista ao select com o set list
      */
 
-    const saveVacancy = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        
-        const localStorageItems: IVacancyProps[] = JSON.parse(localStorage.getItem('vacancyList') || '[]')
-        
-        if(vagaDoForm.id){
-            const vacancyList = localStorageItems.map(itemNoLocalStorage  => {
-                if(itemNoLocalStorage.id === vagaDoForm.id) {
-                    return vagaDoForm
-                }
-                return itemNoLocalStorage
-            })
-            localStorage.setItem('vacancyList', JSON.stringify(vacancyList))
-            setList(vacancyList)
+  const saveVacancy = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
 
-        } else {
-            const newVacancy = {
-                ...vagaDoForm,
-                id: uuidv4()
-            }
-
-            const vacancyList = localStorageItems
-            
-            const vacancyTitleExists = vacancyList.some((item) => {
-                return item.title === newVacancy.title
-            })
-
-            if(vacancyTitleExists){
-                return alert `Este cargo já está cadastrado`
-            }
-
-            localStorage.setItem("vacancyList", JSON.stringify([...vacancyList, newVacancy]))
-            
-            setList((current: IVacancyProps[]) => [...current, newVacancy])
-        }
-        setSelectedVacancy({
-            title: '', 
-            salary: 0,
-            activities: '',
-            benefits: '',
-            steps: '',
-            skills: '',
-            experience: ''
-        } as IVacancyProps)
-    }
-
-    return (
-        <div className={style.container}>
-            <form onSubmit={saveVacancy} className={style.formContainer} id='vacancyForm' autoComplete="off">
-                <Input name='title' labelText='Titulo do Cargo *' value={vagaDoForm.title} change={handleDataChange} required={true} autofocus/> 
-                
-                <Input name='salary' type='number'labelText='Salário *' value={vagaDoForm.salary} change={handleDataChange} required={true}/>
-                
-                <Input name='benefits' labelText='Benefícios' value={vagaDoForm.benefits} change={handleDataChange}/>
-                
-                <Input name='steps' labelText='Etapas' value={vagaDoForm.steps} change={handleDataChange}/>
-            
-                <Input name='skills' labelText='Habilidades' value={vagaDoForm.skills} change={handleDataChange}/>
-            
-                <Input name='experience' labelText='Experiêcia' value={vagaDoForm.experience} change={handleDataChange}/>
-                <div className={style.activities}>
-                    <Input 
-                        name='activities' 
-                        labelText='Atividades do cargo' 
-                        value={vagaDoForm.activities} 
-                        change={handleDataChange}
-                    />
-                </div>
-                <div className={style.buttons}>
-                    <button type="button" onClick={(e)=> VacancyPDF(vagaDoForm)}>
-                        <FilePdf weight="bold" size={18}/>
-                        Gerar PDF
-                    </button>
-                    <button type="submit">Salvar</button>
-                </div>
-            </form>
-        </div>
+    const localStorageItems: IVacancyProps[] = JSON.parse(
+      localStorage.getItem('vacancyList') || '[]',
     )
+
+    if (vagaDoForm.id) {
+      const vacancyList = localStorageItems.map((itemNoLocalStorage) => {
+        if (itemNoLocalStorage.id === vagaDoForm.id) {
+          return vagaDoForm
+        }
+        return itemNoLocalStorage
+      })
+      localStorage.setItem('vacancyList', JSON.stringify(vacancyList))
+      setList(vacancyList)
+    } else {
+      const newVacancy = {
+        ...vagaDoForm,
+        id: uuidv4(),
+      }
+
+      const vacancyList = localStorageItems
+
+      const vacancyTitleExists = vacancyList.some((item) => {
+        return item.title === newVacancy.title
+      })
+
+      if (vacancyTitleExists) {
+        return alert`Este cargo já está cadastrado`
+      }
+
+      localStorage.setItem(
+        'vacancyList',
+        JSON.stringify([...vacancyList, newVacancy]),
+      )
+
+      setList((current: IVacancyProps[]) => [...current, newVacancy])
+    }
+    setSelectedVacancy({
+      title: '',
+      salary: 0,
+      activities: '',
+      benefits: '',
+      steps: '',
+      skills: '',
+      experience: '',
+    } as IVacancyProps)
+  }
+
+  return (
+    <div className={style.container}>
+      <form
+        onSubmit={saveVacancy}
+        className={style.formContainer}
+        id="vacancyForm"
+        autoComplete="off"
+      >
+        <Input
+          name="title"
+          labelText="Titulo do Cargo *"
+          value={vagaDoForm.title}
+          change={handleDataChange}
+          required={true}
+          autofocus
+        />
+
+        <Input
+          name="salary"
+          type="number"
+          labelText="Salário *"
+          value={vagaDoForm.salary}
+          change={handleDataChange}
+          required={true}
+          placeholder="0"
+        />
+
+        <Input
+          name="benefits"
+          labelText="Benefícios"
+          value={vagaDoForm.benefits}
+          change={handleDataChange}
+        />
+
+        <Input
+          name="steps"
+          labelText="Etapas"
+          value={vagaDoForm.steps}
+          change={handleDataChange}
+        />
+
+        <Input
+          name="skills"
+          labelText="Habilidades"
+          value={vagaDoForm.skills}
+          change={handleDataChange}
+        />
+
+        <Input
+          name="experience"
+          labelText="Experiêcia"
+          value={vagaDoForm.experience}
+          change={handleDataChange}
+        />
+        <div className={style.activities}>
+          <Input
+            name="activities"
+            labelText="Atividades do cargo"
+            value={vagaDoForm.activities}
+            change={handleDataChange}
+          />
+        </div>
+        <div className={style.buttons}>
+          <button type="button" onClick={(e) => VacancyPDF(vagaDoForm)}>
+            <FilePdf weight="bold" size={18} />
+            Gerar PDF
+          </button>
+          <button
+            type="submit"
+            onClick={() =>
+              window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+            }
+          >
+            Salvar
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
--- src/pages/VacancyRegister/index.tsx
+++ src/pages/VacancyRegister/index.tsx
@@ -1,59 +1,71 @@
-import { ChangeEvent, useEffect, useState } from 'react';
-import { Form } from '../../components/Form';
-import { Header } from '../../components/Header';
-import { IVacancyProps } from '../../interfaces/IVacancyProps';
-import style from './style.module.css';
+import { ChangeEvent, useEffect, useState } from 'react'
+import { Form } from '../../components/Form'
+import { Header } from '../../components/Header'
+import { IVacancyProps } from '../../interfaces/IVacancyProps'
+import style from './style.module.css'
 
 export const VacancyRegister = () => {
+  const [vacancyList, setVacancyList] = useState<IVacancyProps[]>(
+    [] as IVacancyProps[],
+  )
+  const [selectedVacancy, setSelectedVacancy] = useState<IVacancyProps>(
+    {} as IVacancyProps,
+  )
 
-    const [vacancyList, setVacancyList] = useState<IVacancyProps[]>([] as IVacancyProps[])
-    const [selectedVacancy, setSelectedVacancy] = useState<IVacancyProps>({} as IVacancyProps)
+  // Pega a lista do localStorage e renderiza no select
+  useEffect(() => {
+    const localStorageItems = localStorage.getItem('vacancyList')
+    setVacancyList(localStorageItems ? JSON.parse(localStorageItems) : [])
+  }, [])
 
-    //Pega a lista do localStorage e renderiza no select
-    useEffect(() => {
-        const localStorageItems = localStorage.getItem('vacancyList')
-        setVacancyList(localStorageItems ? JSON.parse(localStorageItems) : [])
-    }, [])
-
-    /*
+  /*
         quando houver mudança dos valores do select, a vaga selecionada será aquela que o id for igual ao 
         value do option do select, e o setSelectedVacancy vai colocar os valores da vaga selecionada dentro do form, ou
         vai inserir valores vazios nele.
     */
-    const handleSelectedChange = (e: ChangeEvent<HTMLSelectElement>) => {
-
-        const selected = vacancyList.find(vacancy => vacancy.id === e.target.value);
-
-        setSelectedVacancy(selected || {
-            title: '', 
-            salary: 0,
-            activities: '',
-            benefits: '',
-            steps: '',
-            skills: '',
-            experience: ''
-        } as IVacancyProps)
-    }
-    return (
-        <>
-            <Header/>
-            <main className={style.mainContainer}>
-                <aside className={style.selectContainer}>
-                    <select  name="selectedVacancy" onChange={handleSelectedChange} value={selectedVacancy.id || 'default'}>
-                        <option value="default">Selecione um modelo</option>
-                        {
-                            vacancyList.length && vacancyList.map(vacancy => (
-                                <option key={vacancy.id} value={vacancy.id}>{vacancy.title}</option>
-                            ))
-                        }
-                    </select>
-                </aside>
-                <Form 
-                    selectedVacancy={selectedVacancy}
-                    setList={setVacancyList}
-                    setSelectedVacancy={setSelectedVacancy}
-                />
-            </main>
-        </>
+  const handleSelectedChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selected = vacancyList.find(
+      (vacancy) => vacancy.id === e.target.value,
     )
+
+    setSelectedVacancy(
+      selected ||
+        ({
+          title: '',
+          salary: 0,
+          activities: '',
+          benefits: '',
+          steps: '',
+          skills: '',
+          experience: '',
+        } as IVacancyProps),
+    )
+  }
+  return (
+    <>
+      <Header />
+      <main className={style.mainContainer}>
+        <aside className={style.selectContainer}>
+          <select
+            name="selectedVacancy"
+            onChange={handleSelectedChange}
+            value={selectedVacancy.id || 'default'}
+          >
+            <option value="default">Selecione um modelo</option>
+            {vacancyList.length &&
+              vacancyList.map((vacancy) => (
+                <option key={vacancy.id} value={vacancy.id}>
+                  {vacancy.title}
+                </option>
+              ))}
+          </select>
+        </aside>
+        <Form
+          selectedVacancy={selectedVacancy}
+          setList={setVacancyList}
+          setSelectedVacancy={setSelectedVacancy}
+        />
+      </main>
+    </>
+  )
 }
